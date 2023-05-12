3214cec3a32dda26eef4bda64fbabb65717e6a8e
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-05-12 16:07:12 +0500

    Specials started

---
+++ schemas/speciala.schema.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose"
+
+// Define the schema for the users table
+const SpecialsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    tap_type: {
+        type: String,
+        required: true
+    },
+    short_desc: {
+        required: true,
+        type: String
+    },
+    long_desc: {
+        required: true,
+        type: String
+    },
+    imgLink: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    res_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Restaurant',
+        required: true
+    }
+});
+
+// Create the users model from the schema
+const Specials = mongoose.model('Specials', SpecialsSchema);
+
+export default Specials;
